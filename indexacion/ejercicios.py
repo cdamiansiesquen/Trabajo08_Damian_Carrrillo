7da26cdb7400edfc8f38a37d8209df18b4918956
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-12-13 12:57:07 -0500

    SE AGREGO LA CARPETA INDEXACION

---
+++ indexacion/ejercicios.py
@@ -0,0 +1,214 @@
+# indexacion 1
+#              10        20        30
+#    0123456789012345678901234567890123
+str="FELIZ NAVIDAD Y PORSPERO AÑO NUEVO"
+
+# Acceso via indice: Variable[idx]
+print(str[8]) #---> V
+print(str[9]) #---> I
+print(str[10]) #---> D
+print(str[11]) #---> A
+print(str[5]) #--->
+print(str[29]) #---> N
+print(str[30]) #---> U
+print(str[31]) #---> E
+print(str[32]) #---> V
+print(str[25]) #---> A
+
+# indexacion 2
+#              10        20        30
+#    01234567890123456789012345678901234567
+str="MORIRE HASTA QUEMAR EL ULTIMO CARTUCHO"
+
+# Acceso via indice: Variable[idx]
+print(str[0]) #---> M
+print(str[14]) #---> U
+print(str[15]) #---> E
+print(str[18]) #---> R
+print(str[25]) #---> T
+print(str[5]) #---> E
+
+# indexacion 3
+#              10        20        30        40        50
+#    01234567890123456789012345678901234567890123456789012345678
+str="ILLIMO TIERRA DEL TUMI DE ORO Y CAPITAL DE LA MIEL DE ABEJA"
+
+# Acceso via indice: Variable[idx]
+print(str[34]) #---> P
+print(str[38]) #---> L
+print(str[37]) #---> A
+print(str[36]) #---> T
+print(str[37]) #---> A
+
+
+# indexacion 4
+#              10        20
+#    0123456789012345678901
+str="EL QUE ESTUDIA TRIUNFA"
+
+# Acceso via indice: Variable[idx]
+print(str[7]) #---> E
+print(str[8]) #---> S
+print(str[9]) #---> T
+print(str[10]) #---> U
+print(str[11]) #---> D
+print(str[12]) #---> I
+print(str[13]) #---> A
+
+# indexacion 5
+#              10
+#    012345678901
+str="DIOS ES AMOR"
+
+# Acceso via indice: Variable[idx]
+print(str[8]) #---> A
+print(str[9]) #---> M
+print(str[10]) #---> O
+print(str[11]) #---> R
+print(str[5]) #---> E
+print(str[6]) #---> S
+
+# indexacion 6
+#              10        20
+#    01234567890123456789012
+str="LA UNION HACE LA FUERZA"
+
+# Acceso via indice: Variable[idx]
+print(str[17]) #---> F
+print(str[18]) #---> U
+print(str[19]) #---> E
+print(str[20]) #---> R
+print(str[21]) #---> Z
+print(str[22]) #---> A
+print(str[8]) #--->
+print(str[3]) #---> U
+print(str[4]) #---> N
+print(str[5]) #---> I
+print(str[11]) #---> C
+print(str[1]) #---> A
+
+
+
+# indexacion 7
+#              10        20
+#    012345678901234567890123456
+str="PIDEME LA LUNA Y TE LA DARE"
+
+# Acceso via indice: Variable[idx]
+print(str[17]) #---> T
+print(str[18]) #---> E
+print(str[20]) #---> L
+print(str[21]) #---> A
+
+
+# indexacion 8
+#              10        20
+#    0123456789012345678901234567890123
+str="AMORES COMO EL NUESTRO NO HAY OTRO"
+
+# Acceso via indice: Variable[idx]
+print(str[15]) #---> N
+print(str[16]) #---> U
+print(str[17]) #---> E
+print(str[18]) #---> S
+print(str[19]) #---> T
+print(str[20]) #---> R
+print(str[21]) #---> O
+print(str[6]) #--->
+print(str[0]) #---> A
+print(str[1]) #---> M
+print(str[2]) #---> O
+print(str[3]) #---> R
+
+# indexacion 9
+#              10        20
+#    0123456789012345678901
+str="EL RITMO DE MI CORAZON"
+
+# Acceso via indice: Variable[idx]
+print(str[5]) #---> T
+print(str[4]) #---> I
+print(str[6]) #---> M
+print(str[7]) #---> O
+print(str[21]) #---> N
+
+# indexacion 10
+#              10        20
+#    0123456789012345678901234567
+str="YO TENGO UN MILLON DE AMIGOS"
+
+# Acceso via indice: Variable[idx]
+print(str[12]) #---> M
+print(str[13]) #---> I
+print(str[14]) #---> L
+print(str[15]) #---> L
+print(str[16]) #---> O
+print(str[17]) #---> N
+
+
+# indexacion 11
+#              10        20
+#    01234567890123456789012345
+str="RECORDAR EL VOLVER A VIVIR"
+
+# Acceso via indice: Variable[idx]
+print(str[21]) #---> V
+print(str[22]) #---> I
+print(str[23]) #---> V
+print(str[24]) #---> I
+print(str[25]) #---> R
+
+# indexacion 12
+#              10
+#    01234567890123456
+str="CAMPEON DEL MUNDO"
+
+# Acceso via indice: Variable[idx]
+print(str[0]) #---> C
+print(str[1]) #---> A
+print(str[2]) #---> M
+print(str[3]) #---> P
+print(str[4]) #---> E
+print(str[5]) #---> O
+print(str[6]) #---> N
+
+# indexacion 13
+#              10
+#    01234567890123456789
+str="SIMBOLO PRECOLOMBINO"
+
+# Acceso via indice: Variable[idx]
+print(str[0]) #---> S
+print(str[1]) #---> I
+print(str[2]) #---> M
+print(str[3]) #---> B
+print(str[4]) #---> O
+print(str[5]) #---> L
+print(str[6]) #---> O
+
+
+# indexacion 14
+#              10
+#    012345678901234567
+str="VIDA YA NO ES VIDA "
+
+# Acceso via indice: Variable[idx]
+print(str[0]) #---> V
+print(str[1]) #---> I
+print(str[2]) #---> D
+print(str[3]) #---> A
+
+
+# indexacion 6
+#              10
+#    01234567890123
+str="CIUDAD HEROICA"
+
+# Acceso via indice: Variable[idx]
+print(str[7]) #---> H
+print(str[8]) #---> E
+print(str[9]) #---> R
+print(str[10]) #---> O
+print(str[11]) #---> I
+print(str[12]) #---> C
+print(str[13]) #---> A
