bfdc5c158d28d3a92161b565148a8b0bd63b30ea
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-12-13 12:56:49 -0500

    SE AGREGO LA CARPETA CORTADO

---
+++ cortado/ejercicios.py
@@ -0,0 +1,105 @@
+# cortado 1
+#                10
+#      01234567890123456
+texto="Biblioteca UNPRG"
+print(texto[0:6]) # --->Biblio
+
+
+# cortado
+#                10
+#      01234567890123456
+texto="Campeon del mundo"
+print(texto[::-1]) # --->odnum led noepmaC
+
+
+
+# cortado 3
+#                10        20
+#      012345678901234567890123456789
+texto="CIENCIAS FISICAS Y MATEMATICAS "
+print(texto[0:8:2]) # --->CECA
+
+
+# cortado 4
+#                10        20
+#      0123456789012345678901234567890123
+texto="TODO LO QUE VALE LA PENA ES DIFICIL "
+print(texto[28:35]) # --->DIFICIL
+
+# cortado 5
+#                10        20
+#      01234567890123456789012345678901
+texto="YO QUISIERA TENERTE A MI LADOOO"
+print(texto[3:11:]) # --->QUISIERA
+
+
+# cortado 6
+#                10        20
+#      0123456789012345678901234567890123456789
+texto="si nos esforzamos todo aquello ser facil"
+print(texto[16:6:-1]) #---->somazrofse
+
+
+# cortado 7
+#                10        20
+#      012345678901234567890123
+texto="Los dioses de olimpoooo"
+print(texto[2::-1]) # --->soL
+
+
+
+# cortado 8
+#                10        20        30        40
+#      01234567890123456789012345678901234567890
+texto="Dios es maravilloso en todos los aspectos"
+print(texto[41:32:-1]) # --->sotcepsa
+
+
+# cortado 9
+#                10        20        30        40        50        60        70
+#      01234567890123456789012345678901234567890123456789012345678901234567890123
+texto="A pesar de que seas lo mejor del mundo, la humildad siempre te hara mejor"
+print(texto[20:38]) # --->lo mejor del mundo
+
+
+# cortado 10
+#                10        20        30        40        50
+#      01234567890123456789012345678901234567890123456789012345678
+texto="Los momentos más sencillos son en realidad los más bonitos"
+print(texto[58:50:-1]) # --->sotinob
+
+# cortado 11
+#                10        20        30
+#      0123456789012345678901234567890123456
+texto="Los amigos de verdad nunca se escapan"
+print(texto[10:3:-1]) # ---> sogima
+
+
+
+# cortado 12
+#                10        20        30        40        50
+#      012345678901234567890123456789012345678901234567890123456789
+texto="ILLIMO TIERRA DEL TUMI DE ORO Y LA CAPITAL DE MIEL DE ABEJA"
+print(texto[53:59] + " " + texto[22:25] + " "+ texto[25:29] ) # ---> ABEJA  DE  ORO
+
+
+
+# cortado 13
+#                10        20        30
+#      0123456789012345678901234567890123456
+texto="Quiñones es el heroe de la fueza area"
+print(texto[26:37]) # --->  fueza area
+
+
+# cortado 14
+#                10        20        30
+#      01234567890123456789012345678901234567
+texto="MORIRE HASTA QUEMAR EL ULTIMO CARTUCHO"
+print(texto[29:38]) # ---> CARTUCHO
+
+
+# cortado 15
+#                10        20        30        40        50
+#      0123456789012345678901234567890123456789012345678901234
+texto="Mi vida es mucho más bonita desde que tú estás en ella"
+print(texto[26:37]) # --->  fueza area
