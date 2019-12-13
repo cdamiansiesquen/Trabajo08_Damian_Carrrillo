5b00f6764896046545012c46f89067e0073cad6d
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-12-13 12:57:24 -0500

    SE AGREGO LA CARPETA ITERACION

---
+++ iteracion/ejercicio.py
@@ -0,0 +1,89 @@
+# iteracion 1
+nombre="ALEXIS "
+for letra in nombre:
+    print(letra)
+
+
+# iteracion 2
+nombre="ELECTRONICA "
+#ITERACION
+for letra in nombre:
+    print(letra)
+
+# iteracion3
+nombre="ARISTOTELES "
+for letra in nombre:
+    print(letra)
+
+# iteracion 4
+FRASE="FElIZ NAVIDAD Y PROSPERO AÑO NUEVO "
+for letra in FRASE:
+    print(letra)
+
+# iteracion 5
+FRASE="LA FELICIDAD LA HACE UNO MISMO "
+for letra in FRASE:
+    print(letra)
+
+
+# iteracion 6
+FRASE="TE QUIERO MUCHO "
+for letra in FRASE:
+    print(letra)
+
+
+# iteracion 7
+nombre="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO "
+for letra in nombre:
+    print(letra)
+
+
+
+# iteracion 8
+nombre="FACULTAD DE CIENCIAS FISICAS Y MATEMATICAS "
+for letra in nombre:
+    print(letra)
+
+
+# iteracion 9
+FRASE="CON ESFUERZO TODO ES POSIBLE "
+for letra in FRASE:
+    print(letra)
+
+# iteracion 10
+nombre="DAMIAN SIESQUEN ALEXIS "
+for letra in nombre:
+    print(letra)
+
+# iteracion 11
+FRASE="ASUME LAS CONSECUENCIAS "
+for letra in FRASE:
+    print(letra)
+
+# iteracion 12
+cancion="YA LO PASADO PASADOOOOOOO "
+for letra in cancion:
+    print(letra)
+
+# iteracion 13
+nombre="TOYOTA "
+for letra in nombre:
+    print(letra)
+
+# iteracion 14
+nombre="PROGRAMACION I "
+for letra in nombre:
+    print(letra)
+
+# iteracion 15
+nombre="ANDROID"
+for letra in nombre:
+    print(letra)
+
+
+
+
+
+
+
+
