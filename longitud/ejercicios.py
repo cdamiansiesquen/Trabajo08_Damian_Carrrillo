1180cb8421f54a79ebdeb8e68d53f95c66be000c
Damian Siesquen <adamiansie@unprg.edu.pe>
2019-12-13 12:57:38 -0500

    SE AGREGO LA CARPETA LONGITUD

---
+++ longitud/ejercicios.py
@@ -0,0 +1,74 @@
+# longitud 1
+nombre="Cristian Alexis Damian Siesquen"
+print(nombre)
+print(len(nombre))
+
+# longitud 2
+nombre="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+print(nombre)
+print(len(nombre))
+
+# lognitud 3
+frase="MORIRE HASTA QUEMAR EL ULTIMO CARTUCHO"
+print(frase)
+print(len(frase))
+
+# longitud 4
+frase="OJOS QUE NO VEN CORAON QUE NO SIENTE"
+print(frase)
+print(len(frase))
+
+# longitud 5
+frase="CUANDO EL RIO SUENA ES PORQUE PIEDRAS TRAE"
+print(frase)
+print(len(frase))
+
+# longitud 6
+nombre="Ingenieria Electronica "
+print(nombre)
+print(len(nombre))
+
+# longitud 7
+nombre="CORONEL FRANCISCO BLOGNESI"
+print(nombre)
+print(len(nombre))
+
+# longitud 8
+facultad="Facultad de ciencias fisicas y matematicas"
+print(facultad)
+print(len(facultad))
+
+# longitud 9
+cancion="YA LO PASADO PASADOOOO NO ME INTERESAAA"
+print(cancion)
+print(len(cancion))
+
+# longitud 10
+cancion="EN BARRANQUILLA ME QUEDOOOOO (EN BARRANQUILA ME QUEDO)"
+print(cancion)
+print(len(cancion))
+
+# longitud 11
+hecho="La revolucion francesa se dio 1789"
+print(hecho)
+print(len(hecho))
+
+#longitud 12
+dato="ME LLAMO ALEXIS , TENGO EL DNI 72520773 Y VIVO EN ILLIMO "
+print(dato)
+print(len(hecho))
+
+# longitud 13
+frase="El todo el planeta hay mas de 7 billones de sonrisas"
+print(frase)
+print(len(frase))
+
+# longitud 14
+frase="LA UNION HACE LA FUERZA"
+print(frase)
+print(len(frase))
+
+# longitud 15
+obra="    EL MUNDO ES ANCHO Y AJENO   "
+print(obra)
+print(len(obra))
