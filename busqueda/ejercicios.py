947a8e0ca19239e33fcd97f175092d2bcb3a4b76
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-12-13 12:54:40 -0500

    SE AGREGO LA CARPETA BUSQUEDA

---
+++ busqueda/ejercicios.py
@@ -0,0 +1,141 @@
+# busqueda1
+#              10        20
+#    012345678901234567890
+str="EL ESTA SOL RADIANTE"
+# UBICANDO LA LETRA DESEA
+print(str.find("O"))
+
+
+# busqueda2
+#              10        20
+#    012345678901234567890
+str="SOLO SE QUE NADA SE"
+# UBICANDO LA LETRA DESEA
+print(str.find("L"))
+
+
+# busqueda3
+#              10        20
+#    012345678901234567890
+str="CONOCETE A TI MISMO"
+# UBICANDO LA PALABRA DESEADA
+print(str.index("CONOCETE"))
+
+
+# busqueda4
+#              10        20        30
+#    01234567890123456789012345678901234
+str="MIGUEL GRAU, CABALLERO DE LOS MARES"
+# UBICANDO LA PALABRA DESEADA
+print(str.index("CABALLERO"))
+
+
+# busqueda5
+#              10        20        30
+#    01234567890123456789012345678901
+str="ANTES DE AMAR, AMATE A TI MISMO"
+# UBICANDO LA PALABRA DESEA
+print(str.find("ALEXIS"))
+
+
+# busqueda6
+#              10        20        30
+#    01234567890123456789012345678901234
+str="ARISTOTELES, GENIO DE LA HUMANIDAD"
+# UBICANDO LA PALABRA DESEA
+print(str.find("AUTO"))
+
+
+# busqueda7
+#              10        20        30        40        50
+#    01234567890123456789012345678901234567890123456789012
+str="Tu sueño nunca se desvanecerá si no le dejas marchar"
+# UBICANDO LA LETRA DESEA
+print(str.find("v"))
+
+
+# busqueda8
+#              10        20
+#    0123456789012345678901
+str="RAYANDO EL SOOOOOOOOOL"
+# UBICANDO LA PALABRA DESEA
+print(str.find("DESEA"))
+
+
+# busqueda9
+#              10        20        30
+#    0123456789012345678901234567890123456789
+str="LA TECNOLOGIA ES EL FUTURO DE LA CIENCIA"
+# UBICANDO LA LETRA DESEA
+print(str.find("PALABRA"))
+
+
+# busqueda10
+#              10        20
+#    0123456789012345678901234567
+str="La mejor salsa es el hambre"
+# UBICANDO LA LETRA DESEA
+print(str.index("h"))
+
+
+
+
+# busqueda11
+#              10        20        30
+#    0123456789012345678901234567890123456789
+str="El amigo de todo el mundo no es un amigo"
+# UBICANDO LA PALABRA DESEA
+print(str.index("t"))
+
+
+
+
+
+
+# busqueda12
+#              10        20        30        40
+#    012345678901234567890123456789012345678901234
+str="ANDRES AVELINO CACERES, EL BRUJO DE LOS ANDES "
+# UBICANDO LA PALABRA DESEA
+print(str.index("CACERES"))
+
+
+
+# busqueda13
+#              10        20        30
+#    012345678901234567890123456789012
+str="SAN MARTIN, EL SANTO DE LA ESPADA"
+# UBICANDO LA PALABRA DESEA
+print(str.find("SANTO"))
+
+
+
+# busqueda14
+#              10        20
+#    012345678901234567890123456
+str="LOS DECLARO MARIDO Y MUJER"
+# UBICANDO LA PALABRA DESEADA
+# COMPROBANDO SI SALE ERROR
+print(str.index("L"))
+
+
+
+# busqueda15
+#              10        20        30        40
+#    01234567890123456789012345678901234567890
+str="EL HOMBRE ES UN SER SOCIAL POR NATURALEZA"
+# UBICANDO LA PALABRA DESEA
+# COMPROBANDO SI SALE ERROR
+print(str.index("MOUSE"))
+
+
+
+
+
+
+
+
+
+
+
+
