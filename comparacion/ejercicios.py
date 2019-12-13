f607d58258714e9bae7214b0dbce3164d9196e82
Damian Siesquen <adamiansie@unprg.edu.pe>
2019-12-13 12:55:28 -0500

    SE AGREGO LA CARPETA COMPARACION

---
+++ comparacion/ejercicios.py
@@ -0,0 +1,82 @@
+# comparacion 1
+cad1="Hola"
+cad2="hola"
+print(cad1 == cad2)
+
+# comparacion 2
+cad01="alexis damian"
+cad02="ALEXIS DAMIAN"
+print(cad02 != cad01)
+
+# comparacion 3
+cad1="AbdAcD"
+cad2="aBDaCd"
+print(cad1 == cad2)
+
+# comparacion 4
+cad1="Hola"
+cad2="Hello"
+print(cad1 == cad2)
+
+# compracion 5
+cad1="JUAN SIESQUEN PADILLA"
+cad2="LUIS JORGE LINARES ALVAREZ"
+print(cad2 > cad1)
+
+#comparacion 6
+cad1="CRISTIAN ALEXIS DAMIAN SIESQUEN"
+cad2="JESUS ARMANDO DAMIAN SIESQUEN"
+print(cad2 > cad1)
+
+
+# comparacion 7
+cad1="ABCDEFG"
+cad2="abcdefg"
+print(cad1 == cad2)
+# comparacion 8
+cad1="MOUSE"
+cad2="MOUSE "
+print(cad1 == cad2)
+
+# comparacion 9
+cad1="PAPEL DE TORNASOL ROJO"
+cad2="PAPEL DE TORNASOL rojo"
+
+print(cad1 == cad2)
+
+
+# comparacion 10
+Cad1="Ingenieria ELectronica"
+Cad2="Ingenieria  Electronica"
+print(Cad1 == Cad2)
+
+# comparacion 11
+Cad01="Programacion"
+Cad02="PrOgraMacioN"
+
+print(cad01 != cad02)
+
+
+# comparacion 12
+frase1="Solo se que nada se"
+frase2="conocete a ti mismo"
+
+print(frase1 != frase2)
+
+# comparacion 13
+dia1=" 12 de diciembre"
+dia2=" 12 de diciembre"
+print(dia1 == dia2)
+
+# comparacion 14
+cad="juan"
+cad1="juan"
+print( cad >= cad1)
+
+
+# comparacion 15
+cad1="DECLARA LA PAZ"
+cad2="DECLARA LA PACE"
+
+print(cad2 < cad1 )
+
