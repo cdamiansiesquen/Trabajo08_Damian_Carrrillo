baca409be4ffa8081ab101291b53458874b50407
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-12-13 12:56:32 -0500

    SE AGREGO LA CARPETA CONCATENACION

---
+++ concatenacion/ejercicios.py
@@ -0,0 +1,99 @@
+#concatenacion 1
+cad1="Alexis"
+cad2="illimo"
+print(cad1+ " " + "vive en" + " "+ cad2 )
+
+# concatenacion 2
+curso="Programacion"
+nota=20
+dia="lunes"
+print("El dia" + " " + dia + " lleve el curso de " + curso + " "+ "y tengo de nota " + str(nota) )
+
+# concatenacion 3
+requisito="programacion I"
+curso="programacion II"
+ciclo="Segundo ciclo"
+print("Requisito:"+ requisito + "\n" + "curso:", curso + " se llevara en el " + ciclo)
+
+# concatenacion 4
+hecho="revolucion francesa"
+año=1789
+print("La"+ " "+ hecho + " " + "se dio en el año " + str(año))
+
+# concatenacion 5
+nombre=">>" + "Alexis"
+edad=">>" + "16 años"
+dni= 72520770
+
+print(nombre + "\n" + edad + "\n" + "DNI es :" + str(dni))
+
+# concatenacion 6
+escuela="Ingenieria Elecronica"
+facultad="Facultad de ciencias de fisicas y matematicas"
+curso="programacion I"
+
+print("El curso de " + curso +  " lo lleva la escuela profesional de "+ escuela + " en la facultad de "+ facultad)
+
+
+# concatenacion 7
+distrito= "Illimo"
+capital="Capital de la miel de abeja"
+region="Lambayeque"
+
+print(distrito + " " + capital + " se encuentra en la region "+ region)
+
+# concatenacion 8
+frase="Solo se que nada se."
+autor="Socrates"
+
+print("La frase: " + frase + " " + "fue atribuida por " + autor)
+
+# concatenacion 9
+region="Lambayeque"
+provincias="Lambayeque, Chiclayo, Ferreñafe"
+numero_Provincias=3
+
+print("La region " + region + " " + "tiene "+ str(numero_Provincias) + " provincias" + " y son:" + provincias)
+
+# concatenacion 10
+numero_de_planetas=8
+galaxia="Via lactea"
+planeta="Planeta Tierra"
+
+print("Nosotros habitamos en el " + planeta + " es uno de los "+ str(numero_de_planetas) + " planetas " + "de la via "+ galaxia)
+
+# concatenacion 11
+curso="Analisis matematico I"
+creditos= 4
+profesor="Andres Figueroa"
+
+print("El curso de ", curso + " Tiene " + str(creditos)+ " creditos " + " y lo dicta el profesor "+ profesor)
+
+# concatenacion 12
+curso="Programacion I"
+creditos= 4
+profesor="Fernando Diaz Sanchez"
+
+print("El curso de ", curso + " Tiene " + str(creditos)+ " creditos " + " y lo dicta el profesor "+ profesor)
+
+# concatenacion 13
+dia=12
+mes="Diciembre"
+año=2019
+
+print("La fecha de hoy es :"+ str(dia) + " de "+ mes + " del año " + str(2109))
+
+# concatenacion 14
+str1="odonto"
+str2="logia"
+nombre="Juan Quiroz"
+año= 15
+
+print("El señor " + nombre + " estudió " + str1 + str2 + " " + "en el año "+ str(año))
+
+# concatenacion 15
+nacimiento="12/11/02"
+nombre="Alexis"
+edad=17
+
+print("El joven "+ nombre + " nacio el "+ nacimiento + " y su edad es "+ str(edad))
